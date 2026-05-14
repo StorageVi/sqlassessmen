03bd16eb911a73d75d58a2ef1ec88296c8b9b08a
StorageVi <24371@burnside.school.nz>
2026-05-14 14:39:52 +1200

    save

--- report.py
+++ report.py
@@ -1,18 +1,24 @@
 #docstring- vincent
 import sqlite3
 DATABASE = 'reportscore'
+db=sqlite3.connect(DATABASE)
+cursor = db.cursor()
 def print_all_score():
-    db=sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = 'SELECT * FROM subject_score'
-    cursor.execute(sql)
+    cursor.execute('SELECT * FROM subject_score')
     results = cursor.fetchall()
     #loop through
-    print(f"{'Week':<10} {'WEEK AVARAGE TOTAL':<10} {'DVC':<5} {'DGT':<5} {'ENG':<5} {'MAT':<5} {'SCI':<5} {'ADP':<5}")
+    print(f"{'WEEK':<16} {'WEEK AVARAGE TOTAL':<30} {'DVC':<10} {'DGT':<10} {'ENG':<10} {'MAT':<10} {'SCI':<10} {'ADP':<10}")
     for score in results:
-        print(f"Week: {score[1]:<10} week avarage total: {score[8]:<10} dvc: {score[2]:<5} dgt: {score[3]:<5} eng: {score[4]:<5} mat:{score[5]:<5} sci: {score[6]:<5} adp: {score[7]:<5}")
-    #loop finish here
-    db.close()
+        print(f"Week: {score[1]:<10} week avarage total: {score[8]:<10} dvc: {score[2]:<5} dgt: {score[3]:<5} eng: {score[4]:<5} mat: {score[5]:<5} sci: {score[6]:<5} adp: {score[7]:<5}")
+user_input = input("What order? \n1(all data weekly), \n2(sort by week avarge) \n5(exit) )")
+while True:
+    if user_input == "1":
+        print_all_score()
+        pass
+    if user_input == "4":
+        pass
+    if user_input == "5":
+        break
+    else:
+        print("Thats was not a valid input try again")
 
-print_all_score()
-order_by = input('Order by what? ')
