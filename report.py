d521e56af134d7cd4707d39a96b715e6b74134f0
StorageVi <24371@burnside.school.nz>
2026-05-15 12:45:16 +1200

    save

--- report.py
+++ report.py
@@ -10,15 +10,26 @@ def print_all_score():
     print(f"{'WEEK':<16} {'WEEK AVARAGE TOTAL':<30} {'DVC':<10} {'DGT':<10} {'ENG':<10} {'MAT':<10} {'SCI':<10} {'ADP':<10}")
     for score in results:
         print(f"Week: {score[1]:<10} week avarage total: {score[8]:<10} dvc: {score[2]:<5} dgt: {score[3]:<5} eng: {score[4]:<5} mat: {score[5]:<5} sci: {score[6]:<5} adp: {score[7]:<5}")
-user_input = input("What order? \n1(all data weekly), \n2(sort by week avarge) \n5(exit) )")
+def print_all_score_by_avarage_total():
+    cursor.execute('SELECT * FROM subject_score ORDER BY avarage_total DESC')
+
+
+
+print("\n1. All data weekly\n2. Sort by week avarge\n5. Exit")
+user_input = int(input("What order? "))
 while True:
-    if user_input == "1":
-        print_all_score()
-        pass
-    if user_input == "4":
-        pass
-    if user_input == "5":
-        break
-    else:
-        print("Thats was not a valid input try again")
+    if user_input == ("") or user_input == (" "):
+        print("Invalid Input.")
+    try
+        if user_input == 1:
+            print_all_score()
+            break
+        elif user_input == "4":
+            pass
+        elif user_input == "5":
+            break
+        else:
+            print("Thats was not a valid input. Try again")
+            break
+    except ValueError:
 
